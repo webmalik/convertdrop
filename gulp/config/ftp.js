52680544c76d689935775bd9ce0604af99e8c2ac
Dmytro Adamovych <ospulyny.it@gmail.com>
2023-05-29 15:45:58 +0300

    edit ftp config

--- gulp/config/ftp.js
+++ gulp/config/ftp.js
@@ -1,10 +1,6 @@
 export let configFTP = {
 	host: "localhost",
 	user: "webmalik",
-<<<<<<< HEAD
-	password: "odino4ka_9_malik",
-=======
 	password: "",
->>>>>>> a48bb5e41976d0619a873da2002b7a6d485cbe96
 	parallel: 5
 }
